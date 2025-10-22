fdd835d8744614f5224501affd8b6613b3e8e21b
chevalier577pro <pelassyalexandre@gmail.com>
2025-10-22 12:55:11 +0200

    Update SESSION_ID, OWNER_NUMBER, and TIMEZONE values

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~SgIUhDyR#taIHTpGMjm9r7-1Yi5w20vLgg3KR6iJeMyAAfZoedX44e1cd7e44f4f8fbd",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "33769730562", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "" //put your country timeZone....leave blank if u don't know.
 };
